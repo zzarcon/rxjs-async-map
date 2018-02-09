d58d2a3dad05915adb9cd0e7929089aaf7d6518a
Sam Reis <sreis@atlassian.com>
2018-02-09 12:36:14 +1100

    reduce array copies in notification loop

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,10 @@ const mapper = <T, U>(project: (value: T) => PromiseLike<U>) => {
   return (value: T) => new Observable<U>(sub => {
     notifiers.push(notify(project(value), sub, () => {
       // notify all notifiers which are ready at the start of the queue
-      while (notifiers.length > 0 && notifiers[0].notifyIfReady()) {
-        // remove notifier after notification has been sent
-        notifiers.shift();
+      const notReadyIdx = notifiers.findIndex(notifier => !notifier.notifyIfReady());
+      if (notReadyIdx > 0) {
+        // remove notifiers after notification has been sent
+        notifiers.splice(0, notReadyIdx);
       }
     }));
   });
