2f08ba57d387099764095ffcddfa7fd0dce2a51e
Sam Reis <sreis@atlassian.com>
2018-02-09 12:55:17 +1100

    create onReady closure outside of mapping function

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,18 @@ import { Notifier, notify } from './notify';
 const mapper = <T, U>(project: (value: T) => PromiseLike<U>) => {
   const notifiers = new Array<Notifier>();
 
+  const onReady = () => {
+    // find the first non-ready notifier in the queue,
+    // while invoking all ready notifiers that we encounter along the way
+    const notReadyIdx = notifiers.findIndex(notifier => !notifier.notifyIfReady());
+    if (notReadyIdx > 0) {
+      // remove all the notifiers we invoked
+      notifiers.splice(0, notReadyIdx);
+    }
+  };
+
   return (value: T) => new Observable<U>(sub => {
-    notifiers.push(notify(project(value), sub, () => {
-      // find the first non-ready notifier in the queue,
-      // while invoking all ready notifiers that we encounter along the way
-      const notReadyIdx = notifiers.findIndex(notifier => !notifier.notifyIfReady());
-      if (notReadyIdx > 0) {
-        // remove all ready notifiers we found, after the notifications have been sent
-        notifiers.splice(0, notReadyIdx);
-      }
-    }));
+    notifiers.push(notify(project(value), sub, onReady));
   });
 };
 
