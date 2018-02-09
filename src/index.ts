84005cdc986ee4812b52258ee41f4a8145a65b26
Sam Reis <sreis@atlassian.com>
2018-02-09 12:47:06 +1100

    clarify comments on notify loop

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,11 @@ const mapper = <T, U>(project: (value: T) => PromiseLike<U>) => {
 
   return (value: T) => new Observable<U>(sub => {
     notifiers.push(notify(project(value), sub, () => {
-      // notify all notifiers which are ready at the start of the queue
+      // find the first non-ready notifier in the queue,
+      // while invoking all ready notifiers that we encounter along the way
       const notReadyIdx = notifiers.findIndex(notifier => !notifier.notifyIfReady());
       if (notReadyIdx > 0) {
-        // remove notifiers after notification has been sent
+        // remove all ready notifiers we found, after the notifications have been sent
         notifiers.splice(0, notReadyIdx);
       }
     }));
