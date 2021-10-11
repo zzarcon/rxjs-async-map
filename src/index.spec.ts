29344ff1e7d11aed59dd0da2e17e87aae319898b
Sam Rijs <srijs@airpost.net>
2021-10-11 21:13:40 +0200

    update rxjs to v7

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,50 +1,59 @@
-import { of, empty } from 'rxjs';
-import { toArray,delay } from 'rxjs/operators';
-import { asyncMap } from './index';
+import { of, empty } from "rxjs";
+import { toArray, delay } from "rxjs/operators";
+import { asyncMap } from "./index";
 
-describe('asyncMap', () => {
-  it('returns empty observable for empty input', async () => {
+describe("asyncMap", () => {
+  it("returns empty observable for empty input", async () => {
     const project = jest.fn();
     const input = empty();
-    const output = await input.pipe(asyncMap(project, 0)).pipe(toArray()).toPromise();
+    const output = await input
+      .pipe(asyncMap(project, 0))
+      .pipe(toArray())
+      .toPromise();
 
     expect(output).toEqual([]);
   });
 
-  it('calls project function in-order for all values in input', async () => {
-    const project = jest.fn(x => Promise.resolve(x.length));
-    const input = of('f', 'ba', 'baz');
-    const output = await input.pipe(asyncMap(project, 1)).pipe(toArray()).toPromise();
+  it("calls project function in-order for all values in input", async () => {
+    const project = jest.fn((x) => Promise.resolve(x.length));
+    const input = of("f", "ba", "baz");
+    const output = await input
+      .pipe(asyncMap(project, 1))
+      .pipe(toArray())
+      .toPromise();
 
     expect(output).toEqual([1, 2, 3]);
-    expect(project.mock.calls).toEqual([['f'], ['ba'], ['baz']]);
+    expect(project.mock.calls).toEqual([["f"], ["ba"], ["baz"]]);
   });
 
-  it('calls project function with the given concurrency', async () => {
-    const invocations: {[v: string]: number} = {};
-    const input = of('foo', 'bar', 'baz');
-    const project = jest.fn(v => {
+  it("calls project function with the given concurrency", async () => {
+    const invocations: { [v: string]: number } = {};
+    const input = of("foo", "bar", "baz");
+    const project = jest.fn((v) => {
       invocations[v] = Date.now();
 
-      return of().pipe(delay(10)).toPromise();
+      return of(42).pipe(delay(10)).toPromise();
     });
     await input.pipe(asyncMap(project, 2)).pipe(toArray()).toPromise();
 
-    expect(invocations['bar'] - invocations['foo']).toBeLessThan(5);
-    expect(invocations['baz']).toBeGreaterThan(invocations['foo']);
-    expect(invocations['baz']).toBeGreaterThan(invocations['bar']);
+    expect(invocations["bar"] - invocations["foo"]).toBeLessThan(5);
+    expect(invocations["baz"]).toBeGreaterThan(invocations["foo"]);
+    expect(invocations["baz"]).toBeGreaterThan(invocations["bar"]);
   });
 
-  it('returns projected values in-order even if promises resolve out of order', async () => {
-    const project = jest.fn(x => {
-      if (x === 'foo') {
+  it("returns projected values in-order even if promises resolve out of order", async () => {
+    const project = jest.fn((x) => {
+      if (x === "foo") {
         return of(1).pipe(delay(20)).toPromise();
       } else {
         return of(2).pipe(delay(10)).toPromise();
       }
     });
-    const input = of('foo', 'bar');
-    const output = await input.pipe(asyncMap(project, 1)).pipe(toArray()).toPromise();
+    const input = of("foo", "bar");
+    const output = await input
+      .pipe(asyncMap(project, 1))
+      .pipe(toArray())
+      .toPromise();
 
     expect(output).toEqual([1, 2]);
   });
--- src/notify.spec.ts
+++ src/notify.spec.ts
@@ -1,38 +1,43 @@
-import { Observable, Observer } from 'rxjs';
-import { expectToReject } from 'jasmine-promise-tools';
-import { notify } from './notify';
+import { Observable, Observer } from "rxjs";
+import { expectToReject } from "jasmine-promise-tools";
+import { notify } from "./notify";
 
 const emptyObserver: Observer<any> = {
-  closed: true,
-  next(value: any): void { /* noop */ },
-  error(err: any): void { throw err; },
-  complete(): void { /*noop*/ }
+  next(value: any): void {
+    /* noop */
+  },
+  error(err: any): void {
+    throw err;
+  },
+  complete(): void {
+    /* noop */
+  },
 };
 
-describe('Notify', () => {
-  it('calls onReady when wrapped promise resolves', async () => {
-    const onReady = jest.fn(p => p.notifyIfReady());
-    await new Observable<void>(sub => {
+describe("Notify", () => {
+  it("calls onReady when wrapped promise resolves", async () => {
+    const onReady = jest.fn((p) => p.notifyIfReady());
+    await new Observable<void>((sub) => {
       notify(Promise.resolve(), sub, onReady);
     }).toPromise();
 
     expect(onReady).toHaveBeenCalledTimes(1);
   });
 
-  it('calls onReady when wrapped promise rejects', async () => {
-    const onReady = jest.fn(p => p.notifyIfReady());
-    const promise = new Observable<void>(sub => {
-      notify(Promise.reject('error'), sub, onReady);
+  it("calls onReady when wrapped promise rejects", async () => {
+    const onReady = jest.fn((p) => p.notifyIfReady());
+    const promise = new Observable<void>((sub) => {
+      notify(Promise.reject("error"), sub, onReady);
     }).toPromise();
     const err = await expectToReject(promise);
 
-    expect(err).toBe('error');
+    expect(err).toBe("error");
     expect(onReady).toHaveBeenCalledTimes(1);
   });
 
-  it('signals non-readiness when the wrapped promise has not been resolved yet', () => {
+  it("signals non-readiness when the wrapped promise has not been resolved yet", () => {
     const onReady = jest.fn();
-    const notifier = notify(Promise.reject('error'), emptyObserver, onReady);
+    const notifier = notify(Promise.reject("error"), emptyObserver, onReady);
 
     expect(notifier.notifyIfReady()).toBe(false);
   });
